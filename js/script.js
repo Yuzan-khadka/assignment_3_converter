dc2044506630f17769a12e4bcc62110a3ecd6deb
Yuzan-khadka <yuzankhadka@gmail.com>
2023-06-16 21:23:21 -0400

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,26 @@
-
-
-
 function convertCurrency(){
-    // var calculation = input1.value * usdToCAD;
-    // input2.value = calculation.toFixed(3);
+    //constant rate
     const usdToCAD = 1.32;
     const cadToUSD = 0.76;
+    // usd value 
     var input1 = document.getElementById('left-input').value;
+    // cad value
     var input2 = document.getElementById('right-input').value;
-    console.log(input1,input2);
-
+    
+    //condition to check input validation
     if(input1 !== null && input2.length == 0){
-        
             input2 = usdToCAD * input1
             document.getElementById("right-input").value  = input2.toFixed(3);
-
-            
        
     }else if(input2 !== null && input1.length == 0){
-        
             input1 = cadToUSD * input2
             document.getElementById("left-input").value  = input1.toFixed(3);
-
     }else{
+        //alert if user tries to input value on both fields
        
             alert("Inappropriate Input. Please Enter value in only one field");
             document.getElementById("left-input").value = "";
             document.getElementById("right-input").value = "";
-
-
-       
     }
 }
 
